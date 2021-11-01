26a6ab218feca34be0e43b451f5d25d9ca52e54b
jayphone17 <m18026629951@163.com>
2021-11-01 10:39:54 +0800

    update Shi-Tomasi🔨

--- Shi-Tomasi角点检测.py
+++ Shi-Tomasi角点检测.py
@@ -12,6 +12,15 @@ for i in corners:
     x,y = i.ravel()
     cv2.circle(img,(x,y),10,250,-1)
 
+# cv2.circle(image, center_coordinates, radius, color, thickness)
+# 参数：
+# image:它是要在其上绘制圆的图像。
+# center_coordinates：它是圆的中心坐标。坐标表示为两个值的元组，即(X坐标值，Y坐标值)。
+# radius:它是圆的半径。
+# color:它是要绘制的圆的边界线的颜色。对于BGR，我们通过一个元组。例如：(255，0，0)为蓝色。
+# thickness:它是圆边界线的粗细像素。厚度-1像素将以指定的颜色填充矩形形状。
+
+
 cv2.namedWindow("Original", 0)
 cv2.resizeWindow("Original" , 480, 640)
 
